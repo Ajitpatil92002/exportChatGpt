deb971377e35d9afe24f445b4a1c0f20aa7c79a5
Ajit Patil <patilajit020@gmail.com>
2023-06-17 18:39:25 +0530

    tutorial added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 import ChatsList from "@/components/ChatsList";
 import ExportGPTSearch from "@/components/ExportGPTSearch";
+import Loading from "@/components/Loading";
 import { Pagination } from "@/components/Pagination";
+import { Suspense } from "react";
 
 async function getData(page) {
   const res = await fetch(`${process.env.BASE_URL}/api/chat-gpt?page=${page}`, {
@@ -15,6 +17,21 @@ async function getData(page) {
 
   return res.json();
 }
+
+const ExportChatGPTVideo = () => {
+  return (
+    <div id="tutorial" className="mt-10 flex items-center flex-col">
+      <span className="text-xl font-semibold text-center mb-4">
+        Exporting Chat GPT Tutorial
+      </span>
+      <iframe
+        src="https://drive.google.com/file/d/15pYECG-hkQItxQ5laZgh83riWsehTLf3/preview"
+        title="Export Chat GPT Tutorial"
+        className="w-[400px] h-[240px] md:w-[600px] md:h-[300px]"
+      ></iframe>
+    </div>
+  );
+};
 
 export default async function Home({ searchParams }) {
   let currentPage = 1;
@@ -38,13 +55,17 @@ export default async function Home({ searchParams }) {
         <p className="desc text-center">
           Export_Chat_GPT is a tool for modern world to discover, share and
           export ChatGPT conversations into a variety of formats. <br /> Whether
-          you need a PDF, images, JSON, MD, HTML, or even audio ,
-          we've got you covered.
+          you need a PDF, images, JSON, MD, HTML, or even audio , we've got you
+          covered.
         </p>
 
         {/* Feed */}
         <section className="feed">
-          <ExportGPTSearch />
+          <Suspense fallback={<Loading />}>
+            <ExportGPTSearch />
+            <ExportChatGPTVideo />
+          </Suspense>
+
           <ChatsList chats={chats} />
         </section>
         <Pagination page={currentPage} pageCount={totalPages} />
--- src/components/ExportGPTSearch.jsx
+++ src/components/ExportGPTSearch.jsx
@@ -2,6 +2,7 @@
 
 import { createChatApi, isMatchingURLPattern } from "@/lib";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
@@ -9,6 +10,7 @@ import { toast } from "react-hot-toast";
 const ExportGPTSearch = () => {
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const router = useRouter();
 
@@ -18,18 +20,21 @@ const ExportGPTSearch = () => {
     if (isMatchingURLPattern(url)) {
       let respdata = await createChatApi(url);
       if (respdata.slug) {
+        setError(false);
         router.push(`chat-gpt/${respdata.slug}`);
         setLoading(false);
         return;
       }
     }
+    router.push(`/#tutorial`);
+    setError(true);
     toast.error("please Enter a valid url");
     setLoading(false);
   };
 
   return (
     <>
-      <form className="relative w-full flex-center" onSubmit={handleSubmit}>
+      <form className="relative w-full flex-center flex-col" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter Your ChatGPT URL"
@@ -40,6 +45,12 @@ const ExportGPTSearch = () => {
           required
           className="search_input peer"
         />
+        {error && (
+          <span className="text-sm p-4 text-red-800 font-semibold">
+            If you are not getting how to export then watch
+            <Link href={`/#tutorial`}> this Tutorial</Link>{" "}
+          </span>
+        )}
       </form>
       {loading && (
         <div className="w-full flex-center">
