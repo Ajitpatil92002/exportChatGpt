f85fedd025b222ca3699b057df4c1c0f6bdc0557
Ajit Patil <patilajit020@gmail.com>
2023-07-14 13:00:35 +0530

    loading component added

--- app/chat-gpt/[slug]/loading.tsx
+++ app/chat-gpt/[slug]/loading.tsx
@@ -2,11 +2,48 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
   return (
-    <div className="flex items-center space-x-4">
-      <Skeleton className="h-12 w-12 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-40 w-[250px]" />
-        <Skeleton className="h-30 w-[200px]" />
+    <div className="prompt_layout mt-5">
+      <div className="prompt_card">
+        <div className="wrapper flex-col justify-start md:flex md:flex-row md:items-center md:gap-5">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="content my-3 ml-5 self-end md:ml-0 md:self-start">
+            <Skeleton className="h-4 w-[250px]" />
+          </div>
+        </div>
+
+        <div className="wrapper mt-8 flex-col justify-start md:flex md:flex-row md:items-center md:gap-5">
+          <div className="answer content my-3 ml-5 self-end md:ml-0 md:self-start">
+            <Skeleton className="h-40 w-[500px]" />
+          </div>
+        </div>
+      </div>
+      <div className="prompt_card">
+        <div className="wrapper flex-col justify-start md:flex md:flex-row md:items-center md:gap-5">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="content my-3 ml-5 self-end md:ml-0 md:self-start">
+            <Skeleton className="h-4 w-[250px]" />
+          </div>
+        </div>
+
+        <div className="wrapper mt-8 flex-col justify-start md:flex md:flex-row md:items-center md:gap-5">
+          <div className="answer content my-3 ml-5 self-end md:ml-0 md:self-start">
+            <Skeleton className="h-40 w-[500px]" />
+          </div>
+        </div>
+      </div>
+      <div className="prompt_card">
+        <div className="wrapper flex-col justify-start md:flex md:flex-row md:items-center md:gap-5">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="content my-3 ml-5 self-end md:ml-0 md:self-start">
+            <Skeleton className="h-4 w-[250px]" />
+          </div>
+        </div>
+
+        <div className="wrapper mt-8 flex-col justify-start md:flex md:flex-row md:items-center md:gap-5">
+          <div className="answer content my-3 ml-5 self-end md:ml-0 md:self-start">
+            <Skeleton className="h-40 w-[500px]" />
+          </div>
+        </div>
       </div>
     </div>
   )
